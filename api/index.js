4dbcad986411b816e0e34f69fbe39d26e82e5edd
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-08-27 14:14:27 -0400

    start api routes

---
+++ api/index.js
@@ -0,0 +1,8 @@
+import Express from 'express';
+const app = Express();
+
+import userRoutes from './routes/users.js';
+
+app.use('/api/users', userRoutes);
+
+app.listen(8000, () => console.log('API Running'));
