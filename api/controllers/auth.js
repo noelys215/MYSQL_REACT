ed6f83bad002ea454e9ffb6037e4472336a0454f
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-08-28 11:46:56 -0400

    add mysql connection

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,9 +1,13 @@
+export const register = (req, res) => {
+	//CHECK IF USER EXISTS
+	//CREATE USER
+	//HASH PASSWORD
+};
+
 export const login = (req, res) => {
 	//TODO
 };
+
 export const logout = (req, res) => {
-	//TODO
-};
-export const register = (req, res) => {
 	//TODO
 };
