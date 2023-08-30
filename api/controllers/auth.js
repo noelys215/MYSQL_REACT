da45151b4fb1d3257b33768156bc39c80bf6f65d
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-08-29 21:00:51 -0400

    continue register function & install bcrypt

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,4 +1,5 @@
 import { db } from '../connect.js';
+import bcrypt from 'bcryptjs';
 
 export const register = (req, res) => {
 	//CHECK IF USER EXISTS
@@ -7,9 +8,20 @@ export const register = (req, res) => {
 	db.query(q, [req.body.username], (err, data) => {
 		if (err) return res.status(500).json(err);
 		if (data.length) return res.status(409).json('User Exists');
+		//CREATE USER
+		//HASH PASSWORD
+		const salt = bcrypt.genSaltSync(10);
+		const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+
+		const q = 'INSERT INTO users (`username`, `email`, `password`, `name`) VALUE (?)';
+
+		const values = [req.body.username, req.body.email, hashedPassword, req.body.name];
+
+		db.query(q, [values], (err, data) => {
+			if (err) return res.status(500).json(err);
+			return res.status(200).json('User Created');
+		});
 	});
-	//CREATE USER
-	//HASH PASSWORD
 };
 
 export const login = (req, res) => {
