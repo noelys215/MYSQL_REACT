33819bf37799cea880263a5d08fe82c19ead233a
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-09-01 12:15:42 -0400

    add jwt, cors and cookie-parser packages

--- api/index.js
+++ api/index.js
@@ -1,14 +1,18 @@
+import cookieParser from 'cookie-parser';
 import express from 'express';
-const app = express();
-
+import cors from 'cors';
+//
 import authRoutes from './routes/auth.js';
 import userRoutes from './routes/users.js';
 import postRoutes from './routes/posts.js';
 import commentsRoutes from './routes/comments.js';
 import likesRoutes from './routes/likes.js';
 
+const app = express();
 // middleware
 app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
