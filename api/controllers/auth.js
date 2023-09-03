e437c3a4828bc9152cda9236e924872df1f46d10
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-09-03 11:00:12 -0400

    finsih logout controller

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -51,5 +51,10 @@ export const login = (req, res) => {
 };
 
 export const logout = (req, res) => {
-	//TODO
+	res.clearCookie('accessToken', {
+		secure: true,
+		sameSite: 'none',
+	})
+		.status(200)
+		.json('User has been logged out.');
 };
