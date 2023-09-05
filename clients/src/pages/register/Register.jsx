bb4fa6ef7fd0c12e90ff33ddafd43a9bfd4d07ef
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-09-04 20:39:39 -0400

    start frontend register function

---
+++ clients/src/pages/register/Register.jsx
@@ -0,0 +1,60 @@
+import { Link } from 'react-router-dom';
+import './register.scss';
+import { useState } from 'react';
+
+const Register = () => {
+	const [inputs, setInputs] = useState({
+		username: '',
+		email: '',
+		password: '',
+		name: '',
+	});
+
+	const handleChange = (e) => setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+	console.log(inputs);
+
+	return (
+		<div className="register">
+			<div className="card">
+				<div className="left">
+					<h1>Lama Social.</h1>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum, alias
+						totam numquam ipsa exercitationem dignissimos, error nam, consequatur.
+					</p>
+					<span>Do you have an account?</span>
+					<Link to="/login">
+						<button>Login</button>
+					</Link>
+				</div>
+				<div className="right">
+					<h1>Register</h1>
+					<form>
+						<input
+							type="text"
+							placeholder="Username"
+							name="username"
+							onChange={handleChange}
+						/>
+						<input
+							type="email"
+							placeholder="Email"
+							name="email"
+							onChange={handleChange}
+						/>
+						<input
+							type="password"
+							placeholder="Password"
+							name="password"
+							onChange={handleChange}
+						/>
+						<input type="text" placeholder="Name" name="name" onChange={handleChange} />
+						<button>Register</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Register;
