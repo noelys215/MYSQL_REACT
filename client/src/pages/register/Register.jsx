89ff14601341af865eef995a6f8e57128abfd2a2
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-09-05 20:43:41 -0400

    add handleClick func

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import './register.scss';
 import { useState } from 'react';
+import axios from 'axios';
 
 const Register = () => {
 	const [inputs, setInputs] = useState({
@@ -9,6 +10,18 @@ const Register = () => {
 		password: '',
 		name: '',
 	});
+	const [err, setErr] = useState(null);
+
+	const handleClick = async (e) => {
+		e.preventDefault();
+
+		try {
+			await axios.post('http://localhost:8800/api/auth/register', inputs);
+		} catch (err) {
+			setErr(err.response.data);
+			console.log(err);
+		}
+	};
 
 	const handleChange = (e) => setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
 
@@ -48,7 +61,8 @@ const Register = () => {
 							onChange={handleChange}
 						/>
 						<input type="text" placeholder="Name" name="name" onChange={handleChange} />
-						<button>Register</button>
+						{err && err}
+						<button onClick={handleClick}>Register</button>
 					</form>
 				</div>
 			</div>
