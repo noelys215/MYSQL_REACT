f3ceafcbcac243111f988d96ef19ada242cedd3a
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-09-08 20:49:30 -0400

    start get posts backend func

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,3 +1,17 @@
+import { db } from '../connect.js';
+import jwt from 'jsonwebtoken';
+import moment from 'moment';
+
 export const getPosts = (req, res) => {
-	//TODO
+	const q =
+		userId !== 'undefined'
+			? `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) WHERE p.userId = ? ORDER BY p.createdAt DESC`
+			: `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId)
+LEFT JOIN relationships AS r ON (p.userId = r.followedUserId) WHERE r.followerUserId= ? OR p.userId =?
+ORDER BY p.createdAt DESC`;
+
+	db.query(q, values, (err, data) => {
+		if (err) return res.status(500).json(err);
+		return res.status(200).json(data);
+	});
 };
