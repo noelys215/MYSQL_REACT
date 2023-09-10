f60b10a92cfef29ba68d45f0a0c76925f354018a
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-09-10 13:00:46 -0400

    finish add post func

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -28,5 +28,23 @@ export const getPosts = (req, res) => {
 };
 
 export const addPost = (req, res) => {
-	//TODO
+	const token = req.cookies.accessToken;
+	if (!token) return res.status(401).json('Not logged in!');
+
+	jwt.verify(token, process.env.JWT_SECRET, (err, userInfo) => {
+		if (err) return res.status(403).json('Token is not valid!');
+
+		const q = 'INSERT INTO posts(`desc`, `img`, `createdAt`, `userId`) VALUES (?)';
+		const values = [
+			req.body.desc,
+			req.body.img,
+			moment(Date.now()).format('YYYY-MM-DD HH:mm:ss'),
+			userInfo.id,
+		];
+
+		db.query(q, [values], (err, data) => {
+			if (err) return res.status(500).json(err);
+			return res.status(200).json('Post has been created.');
+		});
+	});
 };
