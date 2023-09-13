b64b86ccb3c3b59fadca8ae8b7b1aa4a6913e359
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-09-12 20:16:38 -0400

    start share component func

--- client/src/components/share/Share.jsx
+++ client/src/components/share/Share.jsx
@@ -1,49 +1,62 @@
-import "./share.scss";
-import Image from "../../assets/img.png";
-import Map from "../../assets/map.png";
-import Friend from "../../assets/friend.png";
-import { useContext } from "react";
-import { AuthContext } from "../../context/authContext";
+import './share.scss';
+import Image from '../../assets/img.png';
+import Map from '../../assets/map.png';
+import Friend from '../../assets/friend.png';
+import { useContext, useState } from 'react';
+import { AuthContext } from '../../context/authContext';
+// import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 const Share = () => {
+	const [file, setFile] = useState(null);
+	const [desc, setDesc] = useState('');
 
-  const {currentUser} = useContext(AuthContext)
-  return (
-    <div className="share">
-      <div className="container">
-        <div className="top">
-          <img
-            src={currentUser.profilePic}
-            alt=""
-          />
-          <input type="text" placeholder={`What's on your mind ${currentUser.name}?`} />
-        </div>
-        <hr />
-        <div className="bottom">
-          <div className="left">
-            <input type="file" id="file" style={{display:"none"}} />
-            <label htmlFor="file">
-              <div className="item">
-                <img src={Image} alt="" />
-                <span>Add Image</span>
-              </div>
-            </label>
-            <div className="item">
-              <img src={Map} alt="" />
-              <span>Add Place</span>
-            </div>
-            <div className="item">
-              <img src={Friend} alt="" />
-              <span>Tag Friends</span>
-            </div>
-          </div>
-          <div className="right">
-            <button>Share</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	const { currentUser } = useContext(AuthContext);
+
+	const handleClick = async (e) => console.log(file);
+
+	return (
+		<div className="share">
+			<div className="container">
+				<div className="top">
+					<img src={currentUser.profilePic} alt="" />
+					<input
+						type="text"
+						placeholder={`What's on your mind ${currentUser.name}?`}
+						onChange={(e) => setDesc(e.target.value)}
+						value={desc}
+					/>
+				</div>
+				<hr />
+				<div className="bottom">
+					<div className="left">
+						<input
+							type="file"
+							id="file"
+							style={{ display: 'none' }}
+							onChange={(e) => setFile(e.target.files[0])}
+						/>
+						<label htmlFor="file">
+							<div className="item">
+								<img src={Image} alt="" />
+								<span>Add Image</span>
+							</div>
+						</label>
+						<div className="item">
+							<img src={Map} alt="" />
+							<span>Add Place</span>
+						</div>
+						<div className="item">
+							<img src={Friend} alt="" />
+							<span>Tag Friends</span>
+						</div>
+					</div>
+					<div className="right">
+						<button onClick={handleClick}>Share</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Share;
