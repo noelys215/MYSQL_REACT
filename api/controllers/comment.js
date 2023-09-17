7c98f6320b575637c386797566009e4aae62e69f
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-09-16 20:23:32 -0400

    add fetch comment func

--- api/controllers/comment.js
+++ api/controllers/comment.js
@@ -1,3 +1,18 @@
+import { db } from '../connect.js';
+import jwt from 'jsonwebtoken';
+import moment from 'moment';
+
 export const getComments = (req, res) => {
-	//TODO
+	const q = `SELECT c.*, u.id AS userId, name, profilePic FROM comments AS c JOIN users AS u ON (u.id = c.userId)
+    WHERE c.postId = ? ORDER BY c.createdAt DESC
+    `;
+
+	db.query(q, [req.query.postId], (err, data) => {
+		if (err) return res.status(500).json(err);
+		return res.status(200).json(data);
+	});
 };
+
+export const addComment = (req, res) => {};
+
+export const deleteComment = (req, res) => {};
--- api/routes/comments.js
+++ api/routes/comments.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { getComments } from '../controllers/comment.js';
+import { getComments, addComment, deleteComment } from '../controllers/comment.js';
 
 const router = express.Router();
 
-router.get('', getComments);
+router.get('/', getComments);
+router.post('/', addComment);
+router.delete('/:id', deleteComment);
 
 export default router;
