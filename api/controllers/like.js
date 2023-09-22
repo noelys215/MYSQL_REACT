f3767028fa33de5d136f191c2267691dba189c3c
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-09-22 23:00:00 +0900

    add addLike controller

--- api/controllers/like.js
+++ api/controllers/like.js
@@ -1,5 +1,6 @@
 import { db } from '../connect.js';
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv/config';
 
 export const getLikes = (req, res) => {
 	const q = 'SELECT userId FROM likes WHERE postId = ?';
@@ -7,5 +8,22 @@ export const getLikes = (req, res) => {
 	db.query(q, [req.query.postId], (err, data) => {
 		if (err) return res.status(500).json(err);
 		return res.status(200).json(data.map((like) => like.userId));
+	});
+};
+
+export const addLike = (req, res) => {
+	const token = req.cookies.accessToken;
+	if (!token) return res.status(401).json('Not logged in!');
+
+	jwt.verify(token, process.env.JWT_SECRET, (err, userInfo) => {
+		if (err) return res.status(403).json('Token is not valid!');
+
+		const q = 'INSERT INTO likes (`userId`,`postId`) VALUES (?)';
+		const values = [userInfo.id, req.body.postId];
+
+		db.query(q, [values], (err, data) => {
+			if (err) return res.status(500).json(err);
+			return res.status(200).json('Post has been liked.');
+		});
 	});
 };
