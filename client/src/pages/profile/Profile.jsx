3ce7440089ecd624d96661dc54ac9ee2b397ac4c
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-09-28 14:58:52 -0400

    rework jsx in profile page

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -30,61 +30,71 @@ const Profile = () => {
 	const { isLoading: rIsLoading, data: relationshipData } = useQuery(['relationship'], () =>
 		makeRequest.get('/relationships?followedUserId=' + userId).then((res) => res.data)
 	);
+	const handleFollow = () => {};
 
 	return (
 		<div className="profile">
-			<div className="images">
-				<img
-					src="https://images.pexels.com/photos/13440765/pexels-photo-13440765.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-					alt=""
-					className="cover"
-				/>
-				<img
-					src="https://images.pexels.com/photos/14028501/pexels-photo-14028501.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
-					alt=""
-					className="profilePic"
-				/>
-			</div>
-			<div className="profileContainer">
-				<div className="uInfo">
-					<div className="left">
-						<a href="http://facebook.com">
-							<FacebookTwoToneIcon fontSize="large" />
-						</a>
-						<a href="http://facebook.com">
-							<InstagramIcon fontSize="large" />
-						</a>
-						<a href="http://facebook.com">
-							<TwitterIcon fontSize="large" />
-						</a>
-						<a href="http://facebook.com">
-							<LinkedInIcon fontSize="large" />
-						</a>
-						<a href="http://facebook.com">
-							<PinterestIcon fontSize="large" />
-						</a>
+			{isLoading ? (
+				'loading'
+			) : (
+				<>
+					<div className="images">
+						<img src={'/upload/' + data.coverPic} alt="" className="cover" />
+						<img src={'/upload/' + data.profilePic} alt="" className="profilePic" />
 					</div>
-					<div className="center">
-						<span>Jane Doe</span>
-						<div className="info">
-							<div className="item">
-								<PlaceIcon />
-								<span>USA</span>
+					<div className="profileContainer">
+						<div className="uInfo">
+							<div className="left">
+								<a href="http://facebook.com">
+									<FacebookTwoToneIcon fontSize="large" />
+								</a>
+								<a href="http://facebook.com">
+									<InstagramIcon fontSize="large" />
+								</a>
+								<a href="http://facebook.com">
+									<TwitterIcon fontSize="large" />
+								</a>
+								<a href="http://facebook.com">
+									<LinkedInIcon fontSize="large" />
+								</a>
+								<a href="http://facebook.com">
+									<PinterestIcon fontSize="large" />
+								</a>
 							</div>
-							<div className="item">
-								<LanguageIcon />
-								<span>lama.dev</span>
+							<div className="center">
+								<span>{data.name}</span>
+								<div className="info">
+									<div className="item">
+										<PlaceIcon />
+										<span>{data.city}</span>
+									</div>
+									<div className="item">
+										<LanguageIcon />
+										<span>{data.website}</span>
+									</div>
+								</div>
+								{rIsLoading ? (
+									'loading'
+								) : userId === currentUser.id ? (
+									<button onClick={() => setOpenUpdate(true)}>update</button>
+								) : (
+									<button onClick={handleFollow}>
+										{relationshipData.includes(currentUser.id)
+											? 'Following'
+											: 'Follow'}
+									</button>
+								)}
+							</div>
+							<div className="right">
+								<EmailOutlinedIcon />
+								<MoreVertIcon />
 							</div>
 						</div>
-						<button>follow</button>
+						<Posts userId={userId} />
 					</div>
-					<div className="right">
-						<EmailOutlinedIcon />
-						<MoreVertIcon />
-					</div>
-				</div>
-				<Posts />
-			</div>
+				</>
+			)}
+			{openUpdate && <Update setOpenUpdate={setOpenUpdate} user={data} />}
 		</div>
 	);
 };
