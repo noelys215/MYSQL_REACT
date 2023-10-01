8cbf38bc4808e93b5e9b17b3a60c16084d13e9ab
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-10-01 10:29:42 -0400

    start addRelationships controller

--- api/controllers/relationship.js
+++ api/controllers/relationship.js
@@ -1,5 +1,6 @@
 import { db } from '../connect.js';
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv/config';
 
 export const getRelationships = (req, res) => {
 	const q = 'SELECT followerUserId FROM relationships WHERE followedUserId = ?';
@@ -10,6 +11,21 @@ export const getRelationships = (req, res) => {
 	});
 };
 
-export const addRelationship = (req, res) => {};
+export const addRelationship = (req, res) => {
+	const token = req.cookies.accessToken;
+	if (!token) return res.status(401).json('Not logged in!');
+
+	jwt.verify(token, process.env.JWT_SECRET, (err, userInfo) => {
+		if (err) return res.status(403).json('Token is not valid!');
+
+		const q = 'INSERT INTO relationships (`followerUserId`,`followedUserId`) VALUES (?)';
+		const values = [userInfo.id, req.body.userId];
+
+		db.query(q, [values], (err, data) => {
+			if (err) return res.status(500).json(err);
+			return res.status(200).json('Following');
+		});
+	});
+};
 
 export const deleteRelationship = (req, res) => {};
