71c6b7a8c5ae7be8c372797a0416691d5109cf9a
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-10-02 17:14:57 -0400

    add deleteRelationship controller

--- api/controllers/relationship.js
+++ api/controllers/relationship.js
@@ -28,4 +28,18 @@ export const addRelationship = (req, res) => {
 	});
 };
 
-export const deleteRelationship = (req, res) => {};
+export const deleteRelationship = (req, res) => {
+	const token = req.cookies.accessToken;
+	if (!token) return res.status(401).json('Not logged in!');
+
+	jwt.verify(token, process.env.JWT_SECRET, (err, userInfo) => {
+		if (err) return res.status(403).json('Token is not valid!');
+
+		const q = 'DELETE FROM relationships WHERE `followerUserId` = ? AND `followedUserId` = ?';
+
+		db.query(q, [userInfo.id, req.query.userId], (err, data) => {
+			if (err) return res.status(500).json(err);
+			return res.status(200).json('Unfollow');
+		});
+	});
+};
