bffd1cb5af66d9ff71bff1e0aafea96325d3ae01
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-10-05 15:34:21 -0400

    start story controllers

---
+++ api/controllers/story.js
@@ -0,0 +1,10 @@
+import { db } from '../connect.js';
+import jwt from 'jsonwebtoken';
+import moment from 'moment';
+import dotenv from 'dotenv/config';
+
+export const getStories = (req, res) => {};
+
+export const addStory = (req, res) => {};
+
+export const deleteStory = (req, res) => {};
