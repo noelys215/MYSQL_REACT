3eeb0ce8994446475ac451b0477bb3786ff048e0
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-10-11 21:14:55 -0400

    start update component

---
+++ client/src/components/update/Update.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+import { makeRequest } from '../../axios';
+import './update.scss';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+
+export const Update = () => {
+	return <div>Update</div>;
+};
