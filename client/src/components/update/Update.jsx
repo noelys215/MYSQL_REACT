241435a001b68c7babe2841e165535696fc9fd4b
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-10-20 19:58:22 -0400

    finish update.jsx

--- client/src/components/update/Update.jsx
+++ client/src/components/update/Update.jsx
@@ -59,87 +59,82 @@ const Update = ({ setOpenUpdate, user }) => {
 		setOpenUpdate(false);
 		setCover(null);
 		setProfile(null);
-
-		return (
-			<div className="update">
-				<div className="wrapper">
-					<h1>Update Your Profile</h1>
-					<form>
-						<div className="files">
-							<label htmlFor="cover">
-								<span>Cover Picture</span>
-								<div className="imgContainer">
-									<img
-										src={
-											cover
-												? URL.createObjectURL(cover)
-												: '/upload/' + user.coverPic
-										}
-										alt=""
-									/>
-									<CloudUploadIcon className="icon" />
-								</div>
-							</label>
-							<input
-								type="file"
-								id="cover"
-								style={{ display: 'none' }}
-								onChange={(e) => setCover(e.target.files[0])}
-							/>
-							<label htmlFor="profile">
-								<span>Profile Picture</span>
-								<div className="imgContainer">
-									<img
-										src={
-											profile
-												? URL.createObjectURL(profile)
-												: '/upload/' + user.profilePic
-										}
-										alt=""
-									/>
-									<CloudUploadIcon className="icon" />
-								</div>
-							</label>
-							<input
-								type="file"
-								id="profile"
-								style={{ display: 'none' }}
-								onChange={(e) => setProfile(e.target.files[0])}
-							/>
-						</div>
-						<label>Email</label>
-						<input
-							type="text"
-							value={texts.email}
-							name="email"
-							onChange={handleChange}
-						/>
-						<label>Password</label>
-						<input
-							type="text"
-							value={texts.password}
-							name="password"
-							onChange={handleChange}
-						/>
-						<label>Name</label>
-						<input type="text" value={texts.name} name="name" onChange={handleChange} />
-						<label>Country / City</label>
-						<input type="text" name="city" value={texts.city} onChange={handleChange} />
-						<label>Website</label>
-						<input
-							type="text"
-							name="website"
-							value={texts.website}
-							onChange={handleChange}
-						/>
-						<button onClick={handleClick}>Update</button>
-					</form>
-					<button className="close" onClick={() => setOpenUpdate(false)}>
-						close
-					</button>
-				</div>
-			</div>
-		);
 	};
+
+	return (
+		<div className="update">
+			<div className="wrapper">
+				<h1>Update Your Profile</h1>
+				<form>
+					<div className="files">
+						<label htmlFor="cover">
+							<span>Cover Picture</span>
+							<div className="imgContainer">
+								<img
+									src={
+										cover
+											? URL.createObjectURL(cover)
+											: '/upload/' + user.coverPic
+									}
+									alt=""
+								/>
+								<CloudUploadIcon className="icon" />
+							</div>
+						</label>
+						<input
+							type="file"
+							id="cover"
+							style={{ display: 'none' }}
+							onChange={(e) => setCover(e.target.files[0])}
+						/>
+						<label htmlFor="profile">
+							<span>Profile Picture</span>
+							<div className="imgContainer">
+								<img
+									src={
+										profile
+											? URL.createObjectURL(profile)
+											: '/upload/' + user.profilePic
+									}
+									alt=""
+								/>
+								<CloudUploadIcon className="icon" />
+							</div>
+						</label>
+						<input
+							type="file"
+							id="profile"
+							style={{ display: 'none' }}
+							onChange={(e) => setProfile(e.target.files[0])}
+						/>
+					</div>
+					<label>Email</label>
+					<input type="text" value={texts.email} name="email" onChange={handleChange} />
+					<label>Password</label>
+					<input
+						type="text"
+						value={texts.password}
+						name="password"
+						onChange={handleChange}
+					/>
+					<label>Name</label>
+					<input type="text" value={texts.name} name="name" onChange={handleChange} />
+					<label>Country / City</label>
+					<input type="text" name="city" value={texts.city} onChange={handleChange} />
+					<label>Website</label>
+					<input
+						type="text"
+						name="website"
+						value={texts.website}
+						onChange={handleChange}
+					/>
+					<button onClick={handleClick}>Update</button>
+				</form>
+				<button className="close" onClick={() => setOpenUpdate(false)}>
+					close
+				</button>
+			</div>
+		</div>
+	);
 };
 export default Update;
